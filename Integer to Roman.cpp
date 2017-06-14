e0198a01a9f61603a3d2c871e34e243074616fce
Alice <schawxiao@foxmail.com>
2017-06-14 16:18:36 +0800

    Integer to Roman

---
+++ Integer to Roman.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int len=0;
+	int data[5];
+	char number[7]={'I','V','X','L','C','D','M'};
+	string result;
+	string  intToRoman(int num){
+		while(num!=0){
+			data[len++]=num%10;
+			num/=10;
+		}
+		for(int i=0;i<len;i++){
+			int skip=data[i]/5;
+			int cur=data[i]%5;
+			if(data[i]==5){
+				result.push_back(number[2*i+1]);
+
+			}
+			else if(cur<4){
+			    if(skip>0){
+			        result.push_back(number[2*i+1]);
+			    }
+        		for(int j=cur;j>0;j--)
+        		{
+        			result.push_back(number[2*i]);
+    			}
+
+
+			}
+			else if(cur==4){
+			    if(skip>0){
+			        result.push_back(number[2*i]);
+			        result.push_back(number[2*i+2]);
+			    }
+			    else{
+				    result.push_back(number[2*i]);
+				    result.push_back(number[2*i+1]);
+			        
+			    }
+			}
+		}
+		return result;
+	}
+};
+int main(void)
+{
+	Solution s;
+    int n=289;
+    // << "Please input the value of int:" ;
+    //cin >> n;
+
+    string res = s.intToRoman(n);
+    cout << "The roman num is: " << res<<endl;
+
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CPPexercise
+##1.Reverse Words in a String
+##2.Rotate Image
+##3.Gray Code
+##4.Integer to Roman 
